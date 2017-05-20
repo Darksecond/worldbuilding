009a1b7f00f2cce7cf17baeff59492c081c23217
Tim Peters <mail@darksecond.nl>
2017-05-21 00:38:46 +0200

    Vector stuff
    Probably needs a good rewrite to make it faster

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var d = C.new();
-let w = 150;
-let h = 150;
+let w = 050;
+let h = 050;
 d.size(w,h,5);
 for(var y=0;y<h;y++) {
   for(var x=0;x<w;x++) {
-    var n = M.perlin([x/64,y/64])*1 +
-      M.perlin([x/32,y/32])*0.5 +
-      M.perlin([x/16,y/16])*0.25 +
-      M.perlin([x/8,y/8])*0.125;
+    let p = V.v(x/w*256,y/h*256);
+    var n = M.perlin(V.div(p,64))*1 +
+      M.perlin(V.div(p,32))*0.5 +
+      M.perlin(V.div(p,16))*0.25 +
+      M.perlin(V.div(p,8))*0.125;
     var m = n*0.5 + 0.5;
-    d.set([x,y], [m,m,m]);
+    d.set([x,y], V.v(m,m,m));
   }
 }
--- math.js
+++ math.js
@@ -1,18 +1,9 @@
-(function(M){
+(function(M, V){
   'use strict';
 
   M.mix = function(a, b, w) {
     return (1.0 - w) * a + w*b;
   };
-
-  M.dot = function(v1, v2) {
-    if(v1.length != v2.length) throw 'Vectors need to be of the same length';
-    var product = 0;
-    for(var i=0;i<v1.length;i++) {
-      product += v1[i]*v2[i];
-    }
-    return product;
-  }
 
   M.clamp = function(v, lower, upper) {
     if(v < lower) return lower;
@@ -31,23 +22,6 @@
   M.fade = function(t) {
     return t*t*t*(t*(t*6.0 - 15.0) + 10.0);
   }
-
-  M.length = function(v) {
-    var squaredLength = 0;
-    for(var i=0;i<v.length;i++) {
-      squaredLength += v[i]*v[i];
-    }
-    return M.sqrt(squaredLength);
-  }
-
-  M.normalize = function(v) {
-    var length = M.length(v);
-    var v2 = [];
-    for(var i=0;i<v.length;i++) {
-      v2[i] = v[i]/length;
-    }
-    return v2;
-  };
 
   var seed = Math.random();
   M.seed = function(v) {
@@ -78,32 +52,32 @@
   }
 
   M.grad = function(p) {
-    var x = p[0] & 255;
-    var y = p[1] & 255;
+    p = V.v(p);
+    var x = p.x & 255;
+    var y = p.y & 255;
     var g =  perm[x+perm[y]];
     var g2 = grad3[g % 12];
-    return [ g2[0], g2[1] ];
+    return V.v(g2[0], g2[1]);
   };
 
   M.perlin = function(p) {
-    var x = p[0];
-    var y = p[1];
-    var x0 = M.floor(x);
+    p = V.v(p);
+    var x0 = M.floor(p.x);
     var x1 = x0 + 1;
-    var y0 = M.floor(y);
+    var y0 = M.floor(p.y);
     var y1 = y0 + 1;
 
-    var sx = p[0] - x0;
-    var sy = p[1] - y0;
+    var sx = p.x - x0;
+    var sy = p.y - y0;
 
-    var n00 = M.dot(M.grad([x0, y0]), [sx  , sy  ]);
-    var n10 = M.dot(M.grad([x1, y0]), [sx-1, sy  ]);
-    var n01 = M.dot(M.grad([x0, y1]), [sx  , sy-1]);
-    var n11 = M.dot(M.grad([x1, y1]), [sx-1, sy-1]);
+    var n00 = V.dot(M.grad([x0, y0]), [sx  , sy  ]);
+    var n10 = V.dot(M.grad([x1, y0]), [sx-1, sy  ]);
+    var n01 = V.dot(M.grad([x0, y1]), [sx  , sy-1]);
+    var n11 = V.dot(M.grad([x1, y1]), [sx-1, sy-1]);
 
     return M.mix(
         M.mix(n00, n10, M.fade(sx)),
         M.mix(n01, n11, M.fade(sx)),
         M.fade(sy));
   };
-})(window.M = window.M || {})
+})(window.M = window.M || {}, window.V = window.V || {})
